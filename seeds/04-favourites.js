fa8a1648c286ce9e6b6897903786e76077ca9af7
audishos <audisho.sada@gmail.com>
2017-09-28 00:00:21 -0400

    Adds favourites seed file

---
+++ seeds/04-favourites.js
@@ -0,0 +1,22 @@
+
+faker = require('faker');
+
+exports.seed = (knex, Promise) => {
+  // Deletes ALL existing entries
+  return knex('favourites').del()
+    .then( () => {
+      return Promise.all([
+        // Inserts seed entries
+        knex('favourites').insert({user_id: 1, map_id: 3}),
+        knex('favourites').insert({user_id: 1, map_id: 4}),
+        knex('favourites').insert({user_id: 1, map_id: 5}),
+        knex('favourites').insert({user_id: 1, map_id: 6}),
+        knex('favourites').insert({user_id: 2, map_id: 1}),
+        knex('favourites').insert({user_id: 2, map_id: 2}),
+        knex('favourites').insert({user_id: 3, map_id: 1}),
+        knex('favourites').insert({user_id: 3, map_id: 5}),
+        knex('favourites').insert({user_id: 3, map_id: 3}),
+        knex('favourites').insert({user_id: 5, map_id: 5})
+      ]);
+    });
+};
