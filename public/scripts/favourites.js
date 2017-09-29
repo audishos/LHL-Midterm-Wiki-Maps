9bb9559dde0028442eff98e462d8b919e798b5bd
audishos <audisho.sada@gmail.com>
2017-09-29 15:34:51 -0400

    Implements delete favourite from favourites list

--- public/scripts/favourites.js
+++ public/scripts/favourites.js
@@ -1,10 +1,25 @@
-const loadFavourites = () => {
-  $.get("/api/users/favourites")
-  .done( (favourites) => {
-    //jquery to add favourites to page
+function deleteFavourite(deleteContainer) {
+
+  $.ajax({
+    url: "/maps/" + deleteContainer.data("map-id") + "/favourites",
+    type: "DELETE",
+    success: function(result) {
+      deleteContainer.remove();
+    },
+    error: function(error) {
+      console.error(error);
+    }
   });
-};
+
+}
 
 $( () => {
-  loadFavourites();
+  $("#fav-container .fa-trash").on("click", function(ev) {
+
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    deleteFavourite($(ev.target).closest(".fav-row"));
+
+  });
 });
