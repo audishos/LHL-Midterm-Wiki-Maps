a395e0b07b1b080ef02a6d3e3a733788e457b478
audishos <audisho.sada@gmail.com>
2017-09-29 11:46:34 -0400

    Implements add/del favourite routes and helper functions

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -40,20 +40,70 @@ module.exports = function makeDataHelpers(knex){
     },
 
     getUserFavourites: (userId) => {
+
       const promise = new Promise( (resolve, reject) => {
-        knex('maps')
-        .join('favourites', 'maps.id', 'favourites.map_id')
-        .join('users', 'favourites.user_id', 'users.id')
-        .select('maps.*')
-        .where('users.id', userId)
-        .then( (res) => {
-          resolve(res);
-        })
-        .catch( (err) => {
-          reject(err);
-        })
+        if (userId > 0) {
+          knex('maps')
+          .join('favourites', 'maps.id', 'favourites.map_id')
+          .join('users', 'favourites.user_id', 'users.id')
+          .select('maps.*')
+          .where('users.id', userId)
+          .then( (res) => {
+            resolve(res);
+          })
+          .catch( (err) => {
+            reject(err);
+          })
+        } else {
+          reject(`userId: ${userId} must be > 0`);
+        }
       })
+
       return promise;
+
+    },
+
+    addFavourite: (userId, mapId) => {
+
+      const promise = new Promise( (resolve, reject) => {
+        if (userId > 0 && mapId > 0) {
+          knex('favourites').insert({
+            user_id: userId,
+            map_id: mapId
+          })
+          .then( (res) => {
+            resolve(res);
+          })
+          .catch( (err) => {
+            reject(err);
+          });
+        } else {
+          reject(`userId: ${userId} and mapId: ${mapId} must be > 0`);
+        }
+      });
+
+      return promise;
+
+    },
+
+    deleteFavourite: (userId, mapId) => {
+
+      const promise = new Promise( (resolve, reject) => {
+        if (userId > 0 && mapId > 0) {
+          knex('favourites').where('user_id', userId).andWhere('map_id', mapId).del()
+          .then( (res) => {
+            resolve(res);
+          })
+          .catch( (err) => {
+            reject(err);
+          });
+        } else {
+          reject(`userId: ${userId} and mapId: ${mapId} must be > 0`);
+        }
+      });
+
+      return promise;
+
     },
 
     getPointsOnMap: (mapid, callback)=>{
@@ -65,7 +115,7 @@ module.exports = function makeDataHelpers(knex){
         callback(e, null);
       })
     },
-    
+
     //Function to obtain a Map object
     getMapObject: (map_id, callback)=>{
       knex('maps').where('id', map_id).select()
